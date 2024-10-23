c98df2d5184db5b1b9d70321da65174f94450fe8
auspice <zhaokty@gmail.com>
2024-10-22 20:31:49 -0700

    remove unnecessary comments

--- tlsimpl/handshake.py
+++ tlsimpl/handshake.py
@@ -26,18 +26,11 @@ def send_client_hello(sock, key_exchange_pubkey: bytes) -> None:
     base_packet += client_version
     client_random = secrets.token_bytes(32)
     base_packet += client_random
-    # len_session_id = b"\x01"
-    # packet.append(len_session_id)
     session_id = b"\x00"
     base_packet += util.pack_varlen(session_id, 1)
-    # packet.append(session_id)
-    # len_cipher_suites = b"\x02"
-    # packet.append(len_cipher_suites)
     cipher_suites = util.pack(CipherSuite.TLS_AES_256_GCM_SHA384,2)
     base_packet += util.pack_varlen(cipher_suites)
-    # packet.append(cipher_suites)
     comp_methods = b"\x01\x00"
-    # packet.append(comp_methods)
     base_packet += comp_methods
 
     ext = b""
@@ -61,49 +54,6 @@ def send_client_hello(sock, key_exchange_pubkey: bytes) -> None:
     base_packet += util.pack_varlen(ext)
     packet = base_packet
 
-    # keyshare_identifier = b"\x00\x33"
-    # ext.append(keyshare_identifier)
-    # ki_len_data_1 = b"\x00\x26"
-    # ext.append(ki_len_data_1)
-    # ki_len_data_2 = b"\x00\x24"
-    # ext.append(ki_len_data_2)
-    # protocol = b"\x00\x1d"
-    # ext.append(protocol)
-    # len_pubkey = b"\x00\x20"
-    # ext.append(len_pubkey)
-    # ext.append(key_exchange_pubkey)
-
-    # supported_groups = b"\x00\x0a"
-    # ext.append(supported_groups)
-    # sg_len_data_1 = b"\x00\x04"
-    # ext.append(sg_len_data_1)
-    # sg_len_data_2 = b"\x00\x02"
-    # ext.append(sg_len_data_2)
-    # identifiers = b"\x00\x1d"
-    # ext.append(identifiers)
-
-    # supported_versions = b"\x00\x2b"
-    # ext.append(supported_versions)
-    # sv_len_data_1 = b"\x00\x03"
-    # ext.append(sv_len_data_1)
-    # sv_len_data_2 = b"\x02"
-    # ext.append(sv_len_data_2)
-    # sv = b"\x03\x04"
-    # ext.append(sv)
-
-    # sig_algos = b"\x00\x0d"
-    # ext.append(sig_algos)
-    # sa_len_data_1 = b"\x00\x04"
-    # ext.append(sa_len_data_1)
-    # sa_len_data_2 = b"\x00\x02"
-    # ext.append(sa_len_data_2)
-    # algos = b"\x08\x04"
-    # ext.append(algos)
-
-    # len_ext = len(b"".join(ext)).to_bytes(2, "big")
-    # packet.append(len_ext)
-    # for e in ext:
-    #     packet.append(e)
 
     sock.send_handshake_record(HandshakeType.CLIENT_HELLO, packet)
 
